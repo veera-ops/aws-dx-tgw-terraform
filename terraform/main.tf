d5ed6f7a20c915825029500b1ffe442c1c3dd6bf
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-05-31 17:52:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,19 @@ resource "aws_ec2_transit_gateway" "tgw" {
     Name = var.tgw_name
   }
 }
+resource "aws_dx_gateway" "dxgw" {
+
+  name            = var.dxgw_name
+
+  amazon_side_asn = var.amazon_side_asn
+}
+resource "aws_dx_gateway_association" "association" {
+
+  dx_gateway_id         = aws_dx_gateway.dxgw.id
+
+  associated_gateway_id = aws_ec2_transit_gateway.tgw.id
+
+  allowed_prefixes = [
+    "10.0.0.0/8"
+  ]
+}
