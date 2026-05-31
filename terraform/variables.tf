48780fb9a72d72707573a5bee6126e7b2ae6873e
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-05-31 18:04:14 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,14 @@
-variable "aws_region" {}
-variable "terraform_role_arn" {}
+variable "aws_region" {
+  default = "ap-south-1"
+}
 
-variable "tgw_name" {}
-variable "dxgw_name" {}
+variable "tgw_name" {
+  default = "customer-tgw"
+}
+
+variable "dxgw_name" {
+  default = "customer-dxgw"
+}
 
 variable "amazon_side_asn" {
   default = 64512
